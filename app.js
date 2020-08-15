f789737b1e90ce1af105fddd4d8ec6d1a2a80b29
Olivia Pyskoty <olivia.pyskoty@gmail.com>
2020-08-15 16:57:29 -0400

    add methods for coloring squares and restarting game

--- app.js
+++ app.js
@@ -1,19 +1,74 @@
-$(() => {
-  $(".make-color").on("click", function (evt) {
-    let target = evt.target;
-    $(".randomColor").text(randomRgb());
-  });
+let chosenColor;
+
+//generate a color based on random rbg code
+const randomRgb = () => {
+  const r = randNum(0, 255);
+  const g = randNum(0, 255);
+  const b = randNum(0, 255);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+//finds a random number between two numbers
+const randNum = (min, max) => {
+  let num = min + Math.floor(Math.random() * (max - min + 1));
+  return num;
+};
+
+//create the grid
+const fillColorTable = () => {
+  for (let i = 0; i < 2; i++) {
+    let $tr = $("<tr>");
+    for (let j = 0; j < 3; j++) {
+      $tr.append($("<td>").addClass(`box`));
+    }
+    $(".color-grid").append($tr);
+  }
+};
+
+//color squares and pick the matched square
+const fillSquaresAndPickMatch = () => {
+  let colorMatchIdx = randNum(0, 5);
+  let tdArr = $("td").get();
+  tdArr.forEach((td) => $(td).css("background-color", randomRgb()));
+  let colorMatch = tdArr[colorMatchIdx];
+  $(colorMatch).css("background-color", chosenColor);
+};
+
+//check for winner;
+$(".color-grid").on("click", ".box", function (evt) {
+  if ($(evt.target).css("background-color") === chosenColor) {
+    $("#myModal").modal("show");
+  } else {
+    $(".alert").removeClass("hidden");
+
+    setTimeout(function () {
+      $(".alert").addClass("hidden");
+    }, 1000);
+  }
 });
 
+//restart game from modal:
+$(".restartModal").on("click", function () {
+  $("#myModal").modal("hide");
+  location.reload();
+});
 
-const randomRgb = () => {
-  const r = randNum();
-  const g = randNum();
-  const b = randNum();
-  return `rgb(${r},${g},${b})`
-}
+//restart game from container:
+$(".game-buttons").on("click", ".restartGame", function () {
+  location.reload();
+});
 
-const randNum = () => {
-  let num = Math.floor(Math.random() * (255 - 0 + 1));
-  return num;
-}
+$(() => {
+  fillColorTable();
+});
+
+  $(".make-color").on("click", function () {
+    $(".randomColor").text(randomRgb());
+    chosenColor = $(".randomColor").text();
+    fillSquaresAndPickMatch();
+    $(".make-color").prop("disabled", true);
+    let $restartBtn = $(
+      `<button type="button" class="btn btn-warning restartGame">Restart</button>`
+    );
+    $(".game-buttons").append($restartBtn);
+  });
